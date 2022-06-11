61c80ceb87b5d951efb8a4bf50c4327016f99ceb
Yato Aki <yatoakii@gmail.com>
2022-06-11 20:29:51 +0630

    Added Header & Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React,{Component} from 'react';
 import Header from "./components/Header.js"
 import Main from "./components/Main.js"
 import Footer from "./components/Footer.js"
+import "./App.css"
 
 export default class App extends Component {
 
   render(){
     return (
-      <div>
+      <div className="body">
         <Header/>
         <Main/>
         <Footer/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
-import React,{Component} from "react"
+import React,{Component} from "react";
+import "./Header.css"
 
 export default class Header extends Component{
 
   render(){
     return(
-      <div>
-        This is header
+      <div className="header">
+        <span>CV MAKER</span>
       </div>
     );
   }
